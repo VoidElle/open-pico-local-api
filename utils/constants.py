8f5eb1d7118b9d3bf3f3f0c882b70d2821d69461
VoidElle <lucadelc@gmail.com>
2025-11-13 00:24:15 +0100

    chore: Added functions to check if the device's current mode supports fan control and humidity selection

---
+++ utils/constants.py
@@ -0,0 +1,18 @@
+# Preset modes that support the fan speed control
+from enums.device_mode_enum import DeviceModeEnum
+
+MODULAR_FAN_SPEED_PRESET_MODES = [
+    DeviceModeEnum.HEAT_RECOVERY,
+    DeviceModeEnum.EXTRACTION,
+    DeviceModeEnum.IMMISSION,
+    DeviceModeEnum.COMFORT_SUMMER,
+    DeviceModeEnum.COMFORT_WINTER,
+]
+
+# Preset modes that support the selection of a desired level of humidity
+HUMIDITY_SELECTOR_PRESET_MODES = [
+    DeviceModeEnum.HUMIDITY_RECOVERY,
+    DeviceModeEnum.HUMIDITY_EXTRACTION,
+    DeviceModeEnum.CO2_RECOVERY,
+    DeviceModeEnum.CO2_EXTRACTION,
+]
--- utils/functions.py
+++ utils/functions.py
@@ -1,5 +1,8 @@
 from typing import Optional, Dict, Any
 
+from enums.device_mode_enum import DeviceModeEnum
+from enums.on_off_state_enum import OnOffStateEnum
+
 
 def quick_status(ip: str, pin: str, **kwargs) -> Optional[Dict[str, Any]]:
     """
@@ -20,4 +23,28 @@ def quick_status(ip: str, pin: str, **kwargs) -> Optional[Dict[str, Any]]:
     with PicoClient(ip=ip, pin=pin, **kwargs) as iot_device:
         return iot_device.get_status()
 
-    return None
+def device_current_mode_supports_fan_control(mode: DeviceModeEnum, on_off_state: OnOffStateEnum) -> bool:
+    """
+    Check if the given device mode supports fan control.
+
+    Args:
+        :param mode: DeviceModeEnum to check
+        :param on_off_state: OnOffStateEnum to check
+    Returns:
+        True if mode supports fan control and the device is on, False otherwise
+    """
+    from utils.constants import MODULAR_FAN_SPEED_PRESET_MODES
+    return mode in MODULAR_FAN_SPEED_PRESET_MODES and on_off_state == OnOffStateEnum.ON
+
+def device_current_mode_supports_humidity_selection(mode: DeviceModeEnum, on_off_state: OnOffStateEnum) -> bool:
+    """
+    Check if the given device mode supports humidity selection.
+
+    Args:
+        :param mode: DeviceModeEnum to check
+        :param on_off_state: OnOffStateEnum to check
+    Returns:
+        True if mode supports humidity selection and the device is on, False otherwise
+    """
+    from utils.constants import HUMIDITY_SELECTOR_PRESET_MODES
+    return mode in HUMIDITY_SELECTOR_PRESET_MODES and on_off_state == OnOffStateEnum.ON
